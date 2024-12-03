9118d38fd88e5a23c07bc1490fe46988487476a0
Delavuida <jaju1995@naver.com>
2024-12-03 10:06:10 +0900

    #9 게시판 태그 생성/수정/삭제
    코틀린 게시판 개인공부 클론코딩
    게시판 태그 기능 생성, 수정, 삭제 테스트 작성 및 기능 구현

--- board/src/main/kotlin/com/task/board/domain/Post.kt
+++ board/src/main/kotlin/com/task/board/domain/Post.kt
@@ -33,6 +33,13 @@ class Post(
         }
         this.title = postUpdateRequestDto.title
         this.content = postUpdateRequestDto.content
+        replaceTags(postUpdateRequestDto.tags)
         super.updatedBy(postUpdateRequestDto.updatedBy)
+    }
+    private fun replaceTags(tags: List<String>){
+        if(this.tags.map { it.name } != tags) { // 순서 바꾸기 로직이 아닌, map의 객체를 다 비우고, 다시 전체를 넣어준는 방식
+            this.tags.clear()
+            this.tags.addAll(tags.map { Tag(it, this, this.createdBy) })
+        }
     }
 }
